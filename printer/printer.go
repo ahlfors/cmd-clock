8f5c530652341597a0f9599eb5402a4cb6951a05
ahlfors <ahlforslee@proton.me>
2023-11-24 20:03:16 +0800

    remove weather

--- printer/printer.go
+++ printer/printer.go
@@ -5,19 +5,18 @@ import (
 	"os"
 	"time"
 
-	"github.com/lonord/cmd-clock/weather"
 	"golang.org/x/crypto/ssh/terminal"
 )
 
-// Print print clock and weather
-func Print(date time.Time, wea *weather.Weather) error {
+// Print print clock
+func Print(date time.Time) error {
 	clearScreen()
 	w, h, err := terminal.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
 		return err
 	}
 	if w >= largeWidth && h >= largeHeight {
-		printLarge(date, wea, w, h)
+		printLarge(date, w, h)
 		return nil
 	}
 	return errors.New("terminal window size too small")
